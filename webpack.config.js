e479b4ddab16086854b00fbb91ffc0a4099630b0
Hri7566 <hri7566@gmail.com>
2022-10-28 18:21:10 -0400

    1.1.0

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: './index.js',
-    mode: 'development',
+    mode: 'production',
     output: {
         filename: 'cmapi.dist.js',
         path: path.resolve(__dirname, 'dist')
